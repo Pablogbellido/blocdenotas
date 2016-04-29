5f888ccf00c61d88d7830c7a304c71a86e35b4e6
Pablogbellido <pablogbellido@gmail.com>
2016-04-29 14:29:39 +0200

    Bloc de Notas
    Programa con el que podemos abrir, guardar y escribir sobre el fichero
    bloc.txt

---
+++ blocdenotas/blocdenotas/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace blocdenotas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonNuevo = new System.Windows.Forms.Button();
+            this.buttonGuardar = new System.Windows.Forms.Button();
+            this.buttonAbrir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(345, 289);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttonNuevo
+            // 
+            this.buttonNuevo.Location = new System.Drawing.Point(147, 327);
+            this.buttonNuevo.Name = "buttonNuevo";
+            this.buttonNuevo.Size = new System.Drawing.Size(75, 23);
+            this.buttonNuevo.TabIndex = 1;
+            this.buttonNuevo.Text = "Nuevo";
+            this.buttonNuevo.UseVisualStyleBackColor = true;
+            this.buttonNuevo.Click += new System.EventHandler(this.buttonNuevo_Click);
+            // 
+            // buttonGuardar
+            // 
+            this.buttonGuardar.Location = new System.Drawing.Point(282, 328);
+            this.buttonGuardar.Name = "buttonGuardar";
+            this.buttonGuardar.Size = new System.Drawing.Size(75, 23);
+            this.buttonGuardar.TabIndex = 2;
+            this.buttonGuardar.Text = "Guardar";
+            this.buttonGuardar.UseVisualStyleBackColor = true;
+            this.buttonGuardar.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonGuardar_MouseClick);
+            // 
+            // buttonAbrir
+            // 
+            this.buttonAbrir.Location = new System.Drawing.Point(12, 328);
+            this.buttonAbrir.Name = "buttonAbrir";
+            this.buttonAbrir.Size = new System.Drawing.Size(75, 23);
+            this.buttonAbrir.TabIndex = 3;
+            this.buttonAbrir.Text = "Abrir";
+            this.buttonAbrir.UseVisualStyleBackColor = true;
+            this.buttonAbrir.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonAbrir_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 363);
+            this.Controls.Add(this.buttonAbrir);
+            this.Controls.Add(this.buttonGuardar);
+            this.Controls.Add(this.buttonNuevo);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Bloc de Notas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonNuevo;
+        private System.Windows.Forms.Button buttonGuardar;
+        private System.Windows.Forms.Button buttonAbrir;
+    }
+}
+
---
+++ blocdenotas/blocdenotas/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace blocdenotas
+{
+    public partial class Form1 : Form
+    {
+        int i;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGuardar_MouseClick(object sender, MouseEventArgs e)
+        {
+            FileStream fs = new FileStream("bloc.txt", FileMode.Create);
+      
+            StreamWriter sw = new StreamWriter(fs);
+            
+            
+                sw.WriteLine(textBox1.Text);
+            
+            sw.Close();
+            fs.Close();
+        }
+
+        private void buttonNuevo_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void buttonAbrir_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            StreamReader sr = new StreamReader("bloc.txt", Encoding.Default);
+            // textBox1.Text = System.IO.File.ReadAllText("<RutaArchivo>\NombreArchivo");
+                textBox1.Text = File.ReadAllText("bloc.txt");
+            /*
+            while (!sr.EndOfStream)
+            {
+                textBox1.Text = sr.ReadLine() + textBox1.ScrollBars;
+            }
+            */
+            sr.Close();
+
+        }
+    }
+}
